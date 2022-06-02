b27c93e207e8185344b260235f9bb63cc4351fbb
Nhi <thaonhi.nguyen411@gmail.com>
2022-06-02 13:53:57 -0700

    added navigation to header and side

--- javascript/header.js
+++ javascript/header.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
         <link rel="stylesheet" href="../css/navigation.css">
         <nav>
             <ul class="header-navigation">
-                <li class="club-name">Seattle Central Programming Club</li>
+                <li class="club-name">Seattle Central <br> Programming Club</li>
                 <li class="nav-item"><a href="#">Contact</li>
                 <li class="nav-item"><a href="#">Members</li>
                 <li class="nav-item"><a href="#">Socials</li>
