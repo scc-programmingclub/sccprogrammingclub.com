d44386b5871262c85fd39021854c8cfe73821c26
Nhi <thaonhi.nguyen411@gmail.com>
2022-06-16 12:47:31 -0700

    add css icons

--- javascript/aside.js
+++ javascript/aside.js
@@ -8,12 +8,12 @@ class Aside extends HTMLElement {
 
         <nav>
             <ul class="aside-navigation">
-                <li class="nav-item"><a href="../home.html"><i class="fas fa-car"></i>Home</a></li>
-                <li class="nav-item"><a href="#">Contact</a></li>
-                <li class="nav-item"><a href="html/members.html">Members</a>
+                <li class="nav-item"><a href="../home.html"><img class="home_icon" src="../images/css_icons/home.png" alt="home_icon">Home</a></li>
+                <li class="nav-item"><a href="html/contact.html"><img class="contact_icon"src="../images/css_icons/contact.png" alt="contact_icon">Contact</a></li>
+                <li class="nav-item"><a href="#"><img class=members_icon" src="../images/css_icons/members.png" alt="members_icon">Members</a>
                     <ul class="sub-nav-items">
-                        <li class="sub-item"><a href="#">Past Members</a></li>
-                        <li class="sub-item"><a href="#">Current Members</a></li>
+                        <li class="sub-item"><a href="html/past-members.html">Past Members</a></li>
+                        <li class="sub-item"><a href="html/members.html">Current Members</a></li>
                     </ul>
                 </li>
             </ul>  
--- javascript/header.js
+++ javascript/header.js
@@ -8,11 +8,11 @@ class Header extends HTMLElement {
         <nav>
             <ul class="header-navigation">
                 <li class="club-name">Seattle Central <br> Programming Club</li>
-                <li class="nav-item"><a href="#">Contact</a></li>
-                <li class="nav-item"><a href="html/members.html">Members</a>
+                <li class="nav-item"><a href="html/contact.html">Contact</a></li>
+                <li class="nav-item"><a href="#">Members</a>
                     <ul class="sub-nav-items">
-                        <li class="sub-item"><a href="#">Past Members</a></li>
-                        <li class="sub-item"><a href="#">Current Members</a></li>
+                        <li class="sub-item"><a href="html/past-members.html">Past Members</a></li>
+                        <li class="sub-item"><a href="html/contact.html">Current Members</a></li>
                     </ul>
                 </li>
                 <li class="nav-item"><a href="#">Socials</a></li>
