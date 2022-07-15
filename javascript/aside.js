76c8ac6521856e842cddd15b175d93605c8a5bfc
Daniel Johnson <danieldrdj01@gmail.com>
2022-07-14 21:38:12 -0700

    Fixed Links

--- javascript/aside.js
+++ javascript/aside.js
@@ -8,9 +8,9 @@ class Aside extends HTMLElement {
         this.innerHTML = `
         <nav>
             <ul class="aside-navigation">
-                <li class="nav-item" ><a href="/sccprogrammingclub.com/home.html"><img class="home_icon" src="../images/css_icons/home.png" alt="home_icon" style="transform: translateY(20%)">Home</a></li>
-                <li class="nav-item" ><a href="/sccprogrammingclub.com/html/contact.html"><img class="contact_icon" src="../images/css_icons/contact.png" alt="contact_icon" style="transform: translateY(20%)">Contact</a></li>
-                <li class="nav-item" ><a href="#"><img class=members_icon" src="../images/css_icons/members.png" alt="members_icon" style="transform: translateY(20%)">Members</a>
+                <li class="nav-item" ><a href="/sccprogrammingclub.com/home.html"><img class="home_icon" src="/sccprogrammingclub.com/images/css_icons/home.png" alt="home_icon" style="transform: translateY(20%)">Home</a></li>
+                <li class="nav-item" ><a href="/sccprogrammingclub.com/html/contact.html"><img class="contact_icon" src="/sccprogrammingclub.com/images/css_icons/contact.png" alt="contact_icon" style="transform: translateY(20%)">Contact</a></li>
+                <li class="nav-item" ><a href="#"><img class=members_icon" src="/sccprogrammingclub.com/images/css_icons/members.png" alt="members_icon" style="transform: translateY(20%)">Members</a>
                     <ul class="sub-nav-items">
                         <li class="sub-item"><a href="/sccprogrammingclub.com/html/past-members.html">Past Members</a></li>
                         <li class="sub-item"><a href="/sccprogrammingclub.com/html/members.html">Members</a></li>
